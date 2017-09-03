f47c1496eb7fbd525520a4750260ad1421135408
Pawel Piatkowski <cosi1@tlen.pl>
2017-09-03 17:15:04 +0200

    fn_number_cases_spread() - refactoring and documentation

--- R/fn_number_cases_spread.R
+++ R/fn_number_cases_spread.R
@@ -10,15 +10,12 @@
 #' The mean and 95% confidence intervals are obtained by numerically sampling
 #' 10,000 times the incubation and infectious period distributions.
 #'
+#' @param x an \code{epiflows} object
+#' @param code a character string denoting location (country) code
 #' @param time_window_days number of days between the first and last confirmed
 #' disease cases in infectious country
 #' @param num_infec_cases_in_time_window cumulative number of confirmed cases
 #' reported in infectious country in time window W
-#' @param pop_country population of the infectious country
-#' @param annual_travellers_to_other_countries annual number of travellers from
-#' infectious country to other countries
-#' @param annual_travellers_from_other_countries annual number of travellers
-#' from other countries to the infectious country
 #' @param avg_length_stay_days average length of stay in days of travellers from
 #' other countries visiting the infectious country.
 #' This can be an number that is common for all countries or a vector with
@@ -30,17 +27,41 @@
 #'
 #' @return data.frame with the mean and 95% confidence interval of the number
 #' of cases spread to each country
-fn_number_cases_spread <- function(time_window_days,
+#' 
+#' @examples 
+#' flows <- make_epiflows(Mex_travel_2009[[2]], Mex_travel_2009[[1]])
+#' num_countries <- nrow(flows$linelist)
+#' avg_length_stay_days <- rpois(num_countries, 30)
+#' fn_number_cases_spread(
+#'   flows,
+#'   "MEX",
+#'   time_window_days = 365,
+#'   num_infec_cases_in_time_window = 1000,
+#'   avg_length_stay_days = avg_length_stay_days,
+#'   mean_incubation = 4.6,
+#'   var_incubation = 2.7,
+#'   mean_infectious = 4.5,
+#'   var_infectious = 0.6
+#' )
+#' 
+#' @author Paula Moraga
+#' 
+#' @export
+fn_number_cases_spread <- function(x,
+                                   code,
+                                   time_window_days,
                                    num_infec_cases_in_time_window,
-                                   pop_country,
-                                   annual_travellers_to_other_countries,
-                                   annual_travellers_from_other_countries,
                                    avg_length_stay_days,
                                    mean_incubation,
                                    var_incubation,
                                    mean_infectious,
                                    var_infectious) {
 
+  # Get flow and location data from x
+  annual_travellers_to_other_countries <- get_flow_data(x, code, direction = "from")
+  annual_travellers_from_other_countries <- get_flow_data(x, code, direction = "to")
+  pop_country <- get_location_data(x, code)$population
+  
   # Number of countries
   num_countries <- length(annual_travellers_to_other_countries)
   
