2a69896fdc6b58bfbee5e113e7d58e61aafb0416
Thibaut Jombart <thibautjombart@gmail.com>
2018-07-05 15:33:16 +0100

    new version of vis_epiflows

--- R/vis_epiflows.R
+++ R/vis_epiflows.R
@@ -3,7 +3,8 @@
 #' This function shows flows between locations using a dynamic network
 #' visualisation implemented in the package \pkg{visNetwork}, calling the
 #' function [epicontacts::vis_epicontacts()] of the \pkg{epicontacts}
-#' package.
+#' package. The thickness of arrows/edges is proportional to the corresponding
+#' flows.
 #'
 #' @export
 #' @md
@@ -12,14 +13,24 @@
 #'
 #' @param x An `epiflows` object.
 #'
-#' @param ... Further arguments to be passed on to
-#'   [epicontacts::vis_epicontacts()]
+#' @param arrows A logical indicating if arrows should be used to show
+#'   directionality of the flows.
+#'
+#' @param max_width A single number indicating the maximum width of an edge
+#'   (corresponding to the largest flow).
+#'
+#' @param ... Further arguments passed to [epicontacts::vis_epicontacts()]
 #'
 #' @importFrom epicontacts vis_epicontacts
 
-vis_epiflows <- function(x, ...) {
+vis_epiflows <- function(x, arrows = TRUE, max_width = 10, ...) {
 
-  epicontacts::vis_epicontacts(x, ...)
-  
+  edge_width <- get_flows(x)$n
+  edge_width  <- max_width * edge_width / max(edge_width)
+  if (!arrows) {
+    x$directed <- FALSE
+  }
+  out <- epicontacts::vis_epicontacts(x, edge_width = edge_width, ...)
+  out
 }
 
