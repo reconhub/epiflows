8c7daa651fd1169e4e0f4b6cc86cbfcdf5d06c30
Zhian N. Kamvar <zkamvar@gmail.com>
2018-07-05 12:06:27 +0100

    add extra check for get_vars

--- R/get_vars.R
+++ R/get_vars.R
@@ -1,17 +1,19 @@
 #' Access flow data
 #'
-#' This accessor extract location data from an \code{epiflows}
-#' object. \code{get_vars} is a generic with a method defined for
-#' \code{epiflows} objects.
-#'
+#' This accessor extracts variables from the `locations` data frame in an
+#' `epiflow` object. `get_vars` is a generic with a method defined for
+#' `epiflows` objects.
+#' 
+#' The 
 #'
 #' @rdname get_vars
-#' 
+#' @md
+#'
 #' @export
 #'
 #' @author Thibaut Jombart, Zhian Kamvar
 #'
-#' @return A data frame with one 
+#' @return A data frame with one
 
 get_vars <- function(x, ...) {
   UseMethod("get_vars", x)
@@ -23,14 +25,23 @@ get_vars <- function(x, ...) {
 #' @export
 #' 
 #' @param x An \code{epiflows} object.
-#' @param y a valid character string specifying the variable desired. If
+#' @param what a valid character string specifying the variable desired. If
 #'   \code{NULL} (default), the names of the available vars will be returned. 
 
-get_vars.epiflows <- function(x, y = NULL) {
-  if (is.null(y)) {
+get_vars.epiflows <- function(x, what = NULL) {
+  if (is.null(what)) {
     return(x$vars)
+  } 
+  if (!what %in% x$vars) {
+    if (!what %in% names(x$linelist)) {
+      available_vars <- paste(names(x$linelist)[-1], collapse = " ")
+      msg <- paste("%s does not appear to be in the locations data.\n",
+                   "\nThe variables present are:\n%s")
+      msg <- sprintf(msg, what, available_vars)
+      stop(msg)
+    }
   }
-  x$linelist[x$vars[[y]]]
+  x$linelist[x$vars[[what]]]
 }
 
 
--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,7 @@
   epiflows.vars <- c("coords", 
                      "pop_size", 
                      "duration_stay"
-                     )
+  )
   op <- options()
   if (!"epiflows.vars" %in% names(op)) {
     options(epiflows.vars = epiflows.vars)
