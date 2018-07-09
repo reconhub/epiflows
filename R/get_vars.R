7f51845be1dd25d3cdc372c25d1b98ca48721fc3
Zhian N. Kamvar <zkamvar@gmail.com>
2018-07-09 12:55:05 +0100

    Add location arg to get_coords

--- R/get_vars.R
+++ R/get_vars.R
@@ -3,7 +3,7 @@
 #' This accessor extracts variables from the `locations` data frame in an
 #' `epiflow` object. `get_vars` is a generic with a method defined for
 #' `epiflows` objects.
-#' 
+#'
 #' The function `get_coords()` is equivalent to `get_vars(x, "coords", id = TRUE)`
 #'
 #' @rdname get_vars
@@ -21,20 +21,20 @@ get_vars <- function(x, ...) {
 
 
 #' @rdname get_vars
-#' 
+#'
 #' @export
-#' 
+#'
 #' @param x An `epiflows` object.
 #' @param what a valid character string specifying the variable desired. If
 #'   `NULL` (default), the names of the available vars will be returned.
 #' @param id a logical. If `TRUE` (default), the `id` column of the locations
 #'   will be the first column of the data frame. if `FALSE`, the variable will
-#'   be returned without identifiers. 
+#'   be returned without identifiers.
 
 get_vars.epiflows <- function(x, what = NULL, id = TRUE) {
   if (is.null(what)) {
     return(x$vars)
-  } 
+  }
   if (!what %in% names(x$vars)) {
     if (!what %in% names(x$linelist)) {
       available_vars <- paste(names(x$linelist)[-1], collapse = " ")
@@ -58,19 +58,25 @@ get_vars.epiflows <- function(x, what = NULL, id = TRUE) {
 #' @rdname get_vars
 #'
 #' @export
-get_coords <- function(x) {
+get_coords <- function(x, ...) {
   UseMethod("get_coords", x)
 }
 
 
 #' @export
 #' @rdname get_vars
+#' @param location a character specifying a single location to return as a vector of
+#'   coordinates. You cannot specify multiple locations with this parameter.
+#'   Defaults to `NULL`, indicating all locations.
 #'
-get_coords.epiflows <- function(x) {
+get_coords.epiflows <- function(x, location = NULL) {
   res <- try(get_vars(x, "coords", id = TRUE), silent = TRUE)
   if (inherits(res, "try-error")) {
     xprint <- deparse(substitute(x))
     stop(sprintf("coordinates are not set in %s", xprint))
+  }
+  if (is.character(location) && length(location) == 1L) {
+    res <- as.numeric(drop(res[res$id == location, 2:3]))
   }
   res
 }
