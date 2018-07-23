8af62580065bed548d68841b7bea2a57914745fe
Zhian N. Kamvar <zkamvar@gmail.com>
2018-07-23 14:41:46 +0100

    update bracket method
    This one was a bit weird because the handling of
    epicontacts data is weird. If I have this
    inherit the epicontacts class, then it must
    inherit the linelist-first nature of the class,
    which is kind of the opposite of what people
    will want to do with these data.

--- R/handling.R
+++ R/handling.R
@@ -1,36 +1,44 @@
 #' Subset `epiflows` objects
 #'
-#' Returns a subset of an epiflows object based on location code(s).
+#' Returns a subset of an epiflows object.
 #'
-#' @param ef An \code{epiflows} object.
-#' @param i A character vector of one or more location codes.
-#' @param ... Additional parameters (not used).
+#' @param x An \code{epiflows} object.
+#' @param i An integer, logical, or character vector of one or more location.
+#' @param j An integer, logical, or character vector to subset the flows data frame.
+#' @param k A character vector of one or more columns to be retained in the location data.
+#' @param l A character vector of one or more columns to be retained in the flows data frame. Note: if using numbers, the first column stands for the first column after "n".
+#' @param ... Additional parameters passed to \code{\link{[.epicontacts}}.
 #'
 #' @return An \code{epiflows} object.
+#' 
+#' @description An epiflows object inherits the epicontacts class, so the
+#'   subsetting mechanism is also inherited. The benefits is that it's
+#'   extremely flexible. However, this also means that it's possible for
+#'   the contacts to contain IDs that are not present in the locations 
+#'   metadata and vice versa. The best way to consistently subset an
+#'   epiflows object is present in the examples. 
 #'
-#' @author Pawel Piatkowski
+#' @author Zhian N. Kamvar
 #'
 #' @examples
-#' flows <- do.call(make_epiflows, Mex_travel_2009)
-#' flows["MEX"]
+#' data(Brazil_epiflows)
+#' # You can subset, but the flows information will still be present
+#' Brazil_epiflows[j = "Espirito Santo"]
+#' # To help with this, use `thin` from epiflows
+#' epicontacts::thin(flows[j = "Espirito Santo"])
+#' epicontacts::thin(flows[j = c("Espirito Santo", "Rio de Jenerio")])
 #' 
 #' @export
-`[.epiflows` <- function(ef, i, ...) {
-  NextMethod()
-  # if (!is.character(i)) {
-  #   stop("Please specify a character vector of location codes")
-  # }
-  # matching_codes <- i %in% get_codes(ef)
-  # if (!all(matching_codes)) {
-  #   stop("Code not found: ", i[!matching_codes])
-  # }
-  # locationsdata <- get_location_data(ef, i)
-  # to <- get_flow_data(ef, direction = "to")[i]
-  # from <- get_flow_data(ef, direction = "from")[i]
-  # make_epiflows(
-  #   to = to,
-  #   from = from,
-  #   code = ef$origin,
-  #   locationsdata = locationsdata
-  # )
+`[.epiflows` <- function(x, i, j, k = TRUE, l = TRUE, ...) {
+  if (missing(i)) {
+    i <- TRUE
+  }
+  if (missing(j)) {
+    j <- TRUE
+  }
+  # Retaining n
+  l       <- if (is.numeric(l)) l + 1L
+  dots    <- list(...)
+  contact <- if ("contacts" %in% names(dots)) dots$contacts else "either"
+  NextMethod(contacts = contact)
 }
