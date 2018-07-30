98a03f12e66e75d559f105bff45390b7517ac7fb
Zhian N. Kamvar <zkamvar@gmail.com>
2018-07-30 10:53:59 +0100

    add option `all` to get_flows

--- R/get_flows.R
+++ R/get_flows.R
@@ -39,9 +39,11 @@ get_flows <- function(x, ...) {
 #' @param x An `epiflows` object.
 #' @param from a character string defining which regions should be included in the flows
 #' @param to a character string defining which regions should be included in the flows
+#' @param all when `TRUE`, all the columns of the flows data frame will be returned. Defaults to `FALSE`, which returns "from", "to", and "n".
 
-get_flows.epiflows <- function(x, from = NULL, to = NULL, ...) {
-  res <- x$contacts[, c("from", "to", "n"), drop = FALSE]
+get_flows.epiflows <- function(x, from = NULL, to = NULL, all = FALSE, ...) {
+  the_columns <- if (all) TRUE else c("from", "to", "n")
+  res <- x$contacts[, the_columns, drop = FALSE]
   null_from <- is.null(from)
   null_to   <- is.null(to)
   j   <- TRUE
